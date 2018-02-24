6a9271085619e47f1b26a1da3da0b5664708e173
FuChai Mead <mead.fuchai@gmail.com>
2018-02-23 18:42:29 -0700

    react projects

--- js/main.js
+++ js/main.js
@@ -50,3 +50,17 @@ function showone(thechosenone) {
 }
 
 
+const panels = document.querySelectorAll('.panel')
+
+function toggleOpen(){
+  this.classList.toggle('open')
+}
+
+function toggleActive(e){
+  if(e.propertyName.includes('flex')) { 
+    this.classList.toggle('open-active')
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+panels.forEach(panel => panel.addEventListener('transitioned', openActive))
